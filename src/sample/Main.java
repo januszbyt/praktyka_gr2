3f78a780ea506108cb5231068a04118859c2084f
stealthoust <thoustx9@gmail.com>
2021-05-14 12:48:08 +0200

    rejestracja

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("rejestracja.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 650, 776));
         primaryStage.show();
     }
 
--- src/sample/Rejestracja.java
+++ src/sample/Rejestracja.java
@@ -1,28 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Rejestracja implements Initializable {
 
-
-
-
-
-
-
-
-
-
-
-
-
+@FXML
+private ImageView rejestracja_logo;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        File plik= new File("image/logobiale.png");
+        Image zdjecie= new Image(plik.toURI().toString());
+        rejestracja_logo.setImage(zdjecie);
     }
 }
