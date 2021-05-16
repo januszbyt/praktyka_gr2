54901c1497c42390e3b0144601039b39a2e3cded
Jakub Foryś <jakubf1999@gmail.com>
2021-05-17 01:20:03 +0200

    Formularz logowania

---
+++ src/main/java/Logowanie.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Logowanie implements Initializable {
+
+    @FXML
+    private ImageView logowanie_logo;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        File plik= new File("src/images/logobiale.png");
+        Image zdjecie= new Image(plik.toURI().toString());
+        logowanie_logo.setImage(zdjecie);
+    }
+}
