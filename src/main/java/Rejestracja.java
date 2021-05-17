aecd2d728dbbaab94ece62d4cb14e70018ed23bd
stealthoust <thoustx9@gmail.com>
2021-05-17 10:49:25 +0200

    fxml_implement

--- src/main/java/Rejestracja.java
+++ src/main/java/Rejestracja.java
@@ -1,8 +1,13 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import javax.swing.*;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +16,19 @@ public class Rejestracja implements Initializable {
 
     @FXML
     private ImageView rejestracja_logo;
+    @FXML
+    private TextField rejestracja_imie,rejestracja_nazwisko,rejestracja_pesel,rejestracja_login,rejestracja_email;
+    @FXML
+    private PasswordField rejestracja_haslo,rejestracja_haslo2;
+    @FXML
+    private Button btn_zarejestruj,btn_mjk,btn_wyczysc,btn_zamknij;
+    @FXML
+    private Label rejestracja_haslo_alert,rejestracja_alert;
+
+
+
+
+
 
 
     @Override
