f73a4deaafa6f9c430bfdb74c3eb3187771375ab
Jakub Foryś <jakubf1999@gmail.com>
2021-05-18 11:38:17 +0200

    Wprowadzenie klasy odpowiedzialnej za powiadomienia

---
+++ src/main/java/Powiadomienia.java
@@ -0,0 +1,42 @@
+import javafx.scene.control.Alert;
+
+public class Powiadomienia
+
+{
+
+    // Rejestracja
+    public void alertRejestracja(String blad)
+    {
+        //sprawdzenie czy zmienna przechowywujace nieprawidlowe pola jest pusta
+
+        if(blad.isBlank())
+        {//SUKCES
+            Alert sukces = new Alert(Alert.AlertType.INFORMATION);
+            sukces.setTitle("Powiadomienie");
+            sukces.setHeaderText(null);
+            sukces.setContentText("Rejestracja zakończona sukcesem!");
+
+            sukces.showAndWait();
+        }
+        else
+        {//PORAZKA
+            Alert porazka = new Alert(Alert.AlertType.ERROR);
+            porazka.setTitle("Powiadomienie");
+            porazka.setHeaderText(null);
+            porazka.setContentText("Rejestracja zakończona niepowodzeniem. Popraw następujące pola: "+blad );
+
+            porazka.showAndWait();
+        }
+
+    }
+
+
+    // Logowanie
+
+
+
+    // Przelew
+
+
+
+}
