f50bc95ee9aa1e1490357e3e5a38acd156ce3de6
Marcin Botwina <mbotwina2@gmail.com>
2021-05-20 16:10:13 +0200

    Utworzenie klasy odpowiedzialnej za hashowanie i sprawdzanie hasla

---
+++ src/main/java/classes/Hash.java
@@ -0,0 +1,13 @@
+package classes;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class Hash {
+    public static String getHash(String haslo){
+        return BCrypt.withDefaults().hashToString(12, haslo.toCharArray());
+    }
+    public static boolean checkHash(String haslo, String hash){
+        BCrypt.Result x = BCrypt.verifyer().verify(haslo.toCharArray(), hash);
+        return x.verified;
+    }
+
+}
