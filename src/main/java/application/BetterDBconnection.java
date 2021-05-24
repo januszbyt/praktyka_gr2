936c6d929970744ebc744b404e9382ec1cb1dee1
Marcin Botwina <mbotwina2@gmail.com>
2021-05-24 12:39:47 +0200

    Implementacja połączenia z bazą danych v2.0

---
+++ src/main/java/application/BetterDBconnection.java
@@ -0,0 +1,69 @@
+package application;
+import java.sql.*;
+
+public class BetterDBconnection {
+
+    static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://sql4.freemysqlhosting.net/sql4412131";
+    static final String DB_USER = "sql4412131";
+    static final String DB_PASSWORD = "azsNhGPJfq";
+    Connection connection;
+
+    public BetterDBconnection(){
+        connect();
+    }
+
+    public void connect(){
+        try{
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        }catch(SQLException|ClassNotFoundException e){
+            System.out.println("ERROR connecting to database!");
+            System.out.println(e.toString());
+        }
+    }
+
+    public ResultSet select(String query){
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            return result;
+        }catch(SQLException e){
+            System.out.println("ERROR while executing select query!");
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+    public int update(String query){
+        try{
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(query);
+        }catch(SQLException e){
+            System.out.println("ERROR while executing update query");
+            System.out.println(e.toString());
+            return -1;
+        }
+    }
+
+    public int delete(String query){
+        try{
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(query);
+        }catch(SQLException e){
+            System.out.println("ERROR while deleting line!");
+            System.out.println(e.toString());
+            return -1;
+        }
+    }
+
+    public void close(){
+        try{
+            connection.close();
+        }catch(SQLException e){
+            System.out.println("ERROR while closing connections!");
+            System.out.println(e.toString());
+        }
+    }
+
+}
