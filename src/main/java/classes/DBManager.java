55ec9872fe845e21c165b0aadd5e3f1c9262471a
Marcin Botwina <mbotwina2+git@gmail.com>
2021-05-25 09:33:32 +0200

    Dodanie klasy DBManager

---
+++ src/main/java/classes/DBManager.java
@@ -0,0 +1,21 @@
+package classes;
+import application.BetterDBConnection;
+
+import java.sql.ResultSet;
+
+public class DBManager {
+
+
+   public static void update(String query){
+       new BetterDBConnection().update(query);
+   }
+
+   public static ResultSet select(String query){
+      return new BetterDBConnection().select(query);
+   }
+
+   public static void delete(String query){
+       new BetterDBConnection().delete(query);
+   }
+
+}
