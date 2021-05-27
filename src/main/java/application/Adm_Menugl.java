aab9557a50da42f504ecba099cb58bdda69c52af
Fidek-creator <83137313+Fidek-creator@users.noreply.github.com>
2021-05-27 13:40:29 +0200

    Dodanie formularza i kontrolera administratora

---
+++ src/main/java/application/Adm_Menugl.java
@@ -0,0 +1,68 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Adm_Menugl implements Initializable {
+    public ImageView menugl_logo;
+    public Button btn_adm_zarzadzaj_uz;
+    public Button btn_adm_zarzadzaj_wal;
+    public Button btn_adm_zgloszenia;
+    public Button btn_adm_historia;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        File plik = new File("src/images/logobiale.png");
+        Image zdjecie = new Image(plik.toURI().toString());
+        menugl_logo.setImage(zdjecie);
+    }
+
+
+    public void btn_adm_zarzadzaj_uz_M(ActionEvent actionEvent)  {
+        try
+        {
+            ZmienOkno.zmienScene("adm_zarzadzanie_uzytkownikami.fxml", 750, 483, btn_adm_zarzadzaj_uz);
+        }
+        catch (Exception e){
+            System.out.println("Błąd w wczytaniu okna");
+        }
+    }
+
+    public void btn_adm_zarzadzaj_wal_M(ActionEvent actionEvent) {
+        try
+        {
+            ZmienOkno.zmienScene("adm_zarządzaj_wal.fxml", 750, 483, btn_adm_zarzadzaj_wal);
+        }
+        catch (Exception e){
+            System.out.println("Błąd w wczytaniu okna");
+        }
+    }
+
+    public void btn_adm_zgloszenia_M(ActionEvent actionEvent) {
+        try
+        {
+            ZmienOkno.zmienScene("adm_zgloszenia.fxml", 791, 522, btn_adm_zgloszenia);
+        }
+        catch (Exception e){
+            System.out.println("Błąd w wczytaniu okna");
+        }
+    }
+
+    public void btn_adm_historia_M(ActionEvent actionEvent) {
+        try
+        {
+            ZmienOkno.zmienScene("adm_historia.fxml", 1077, 534, btn_adm_historia);
+        }
+        catch (Exception e){
+            System.out.println("Błąd w wczytaniu okna");
+        }
+    }
+}
---
+++ src/main/java/application/Adm_Zarzadzanie_uzytkownikami.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Adm_Zarzadzanie_uzytkownikami implements Initializable {
+    public ImageView img_menugl;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //wczytanie obrazu
+        File plik = new File("src/images/domek.png");
+        Image zdjecie = new Image(plik.toURI().toString());
+        img_menugl.setImage(zdjecie);
+    }
+    public void img_menugl_M(MouseEvent mouseEvent) throws Exception {
+        ZmienOkno.zmienSceneimg("adm_menugl.fxml", 1077, 534, img_menugl);
+    }
+}
